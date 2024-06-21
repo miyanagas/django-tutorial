59da969699500667c0facd21b3e4edd68fbe2d01
miyanagas <zs0h8po2t7ad@gmail.com>
2024-06-21 18:38:33 +0900

    post_detailni formを追加で表示

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -1,6 +1,7 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import CommentForm
 
 def frontpage(request):
     posts = Post.objects.all()
@@ -8,4 +9,18 @@ def frontpage(request):
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
-    return render(request, "blog/post_detail.html", {"post": post})
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect("post_detail", slug=post.slug)
+
+    else:
+        form = CommentForm()
+
+    return render(request, "blog/post_detail.html", {"post": post, "form": form})
